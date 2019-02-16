80a539c94c21b3bb1817325a2686dde255522a56
shayan6 <42208796+shayan6@users.noreply.github.com>
2019-02-16 19:00:54 +0500

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,98 @@
+var questions = 
+      [
+        {
+          "question": "If Shayan Meet with Genie what should be Shayan's wish?",
+          "option1": "A Billion Of Dollors",
+          "option2": "Beautiful Wife", 
+          "option3": "Become most handsome man in the world", 
+          "option4": "To be President/Prime Minister",
+          "answer": "1"
+        },
+        
+        {
+          "question": "What Does Shayan Drink The Most?",
+          "option1": "Tea",
+          "option2": "Cofee", 
+          "option3": "Coke", 
+          "option4": "Alcohol",
+          "answer": "1"
+        },
+
+        {
+          "question": "Where Would Shayan Like To Go With Shayan's SoulMate?",
+          "option1": "London",
+          "option2": "Paris", 
+          "option3": "New York", 
+          "option4": "Kashmir",
+          "answer": "4"
+        },
+
+        {
+          "question": "Which One id Shayan's Favourite PSL team?",
+          "option1": "Karachi Kings",
+          "option2": "Islamabad United", 
+          "option3": "Peshawar Zalmi", 
+          "option4": "Quetta Gladiators",
+          "answer": "2"
+        },
+
+        {
+          "question": "Which Animal Does Shayan's Dream Pet?",
+          "option1": "A Dog",
+          "option2": "A Cat", 
+          "option3": "Baby Tiger", 
+          "option4": "Gold Fish",
+          "answer": "3"
+        },
+
+
+        {
+          "question": "Which One is The Shayan's Favourite Movie?",
+          "option1": "Titanic",
+          "option2": "The Conjuring", 
+          "option3": "The Avengers", 
+          "option4": "Harry Potter",
+          "answer": "3"
+        },
+
+
+        {
+          "question": "In Free Time, What Would Shayan do?",
+          "option1": "Sketching",
+          "option2": "Listen Songs", 
+          "option3": "Programing", 
+          "option4": "Watch Movie",
+          "answer": "2"
+        },
+
+
+        {
+          "question": "What Would Shayan Do If Shayan Won A lottery?",
+          "option1": "Travel The World",
+          "option2": "Invest It", 
+          "option3": "Buy A Sport Car", 
+          "option4": "Donate",
+          "answer": "4"
+        },
+
+        {
+          "question": "Which One Is The Shayan's Favourite Celebrity?",
+          "option1": "Micheal Jackson",
+          "option2": "Jonny Deep", 
+          "option3": "Saleena Gomez", 
+          "option4": "Robert Downey Jr.",
+          "answer": "1"
+        },
+
+
+        {
+          "question": "Which one is the Shayan's Favourite Color?",
+          "option1": "Red", 
+          "option2": "Yellow", 
+          "option3": "Blue",
+          "option4": "Light Green",
+          "answer": "4"
+        }
+
+
+        ];
